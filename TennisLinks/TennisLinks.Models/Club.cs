7d4ec0fc5977bd94f3fd9a7949abff7725a51ee2
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:19:13 +0300

    created club model

---
+++ TennisLinks/TennisLinks.Models/Club.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TennisLinks.Models.Enumerations;
+using TennisLinks.Models.Interfaces;
+
+namespace TennisLinks.Models
+{
+    public class Club : IEntity
+    {
+        private ICollection<User> users;
+
+        public Club()
+        {
+            this.users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public Surface SurfaceType { get; set; }
+
+        public int CityId { get; set; }
+
+        public virtual City City { get; set; }
+
+        public virtual ICollection<User> Users
+        {
+            get { return this.users; }
+            set { this.users = value; }
+        }
+    }
+}
