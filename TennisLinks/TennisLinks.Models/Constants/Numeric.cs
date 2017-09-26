f571c987976a9513fa7b6d3e87e1f07db712a5a3
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:19:39 +0300

    added constants for skill levels

---
+++ TennisLinks/TennisLinks.Models/Constants/Numeric.cs
@@ -0,0 +1,21 @@
+﻿namespace TennisLinks.Models.Constants
+{
+    public class Numeric
+    {
+        public static readonly double[] Skill =
+        {
+            1,
+            1.5,
+            2,
+            2.5,
+            3,
+            3.5,
+            4,
+            4.5,
+            5,
+            5.5,
+            6,
+            7
+        };
+    }
+}
