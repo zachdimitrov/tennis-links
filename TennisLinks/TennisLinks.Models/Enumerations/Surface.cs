70cab0f9a3f8638914a3b9c6c730effed61e6b9a
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:20:34 +0300

    added Enumerations - surfaces - gender - time of day

---
+++ TennisLinks/TennisLinks.Models/Enumerations/Surface.cs
@@ -0,0 +1,12 @@
+﻿namespace TennisLinks.Models.Enumerations
+{
+    public enum Surface
+    {
+        Clay = 1,
+        Hard = 2,
+        Carpet = 3,
+        Indoor = 4,
+        Asphalt = 5,
+        Grass = 6,
+    }
+}
---
+++ TennisLinks/TennisLinks.Models/Enumerations/TimeOfDay.cs
@@ -0,0 +1,11 @@
+﻿namespace TennisLinks.Models.Enumerations
+{
+    public enum TimeOfDay
+    {
+        morning = 1, 
+        lunch = 2,
+        afternoon = 3,
+        evening = 4,
+        night = 5
+    }
+}
