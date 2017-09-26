7af886151514bd21f56a99022fc8942a8fb20b00
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:21:36 +0300

    added playtime model

---
+++ TennisLinks/TennisLinks.Models/PlayTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TennisLinks.Models.Enumerations;
+
+namespace TennisLinks.Models
+{
+    public class PlayTime
+    {
+        private ICollection<User> users;
+
+        public PlayTime()
+        {
+            this.Id = (int)this.Time;
+            this.users = new HashSet<User>();
+        }
+
+        [Key]
+        public int Id;
+
+        [Required]
+        public TimeOfDay Time { get; set; }
+
+        public virtual ICollection<User> Users
+        {
+            get { return this.users; }
+            set { this.users = value; }
+        }
+    }
+}
