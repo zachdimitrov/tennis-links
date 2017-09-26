9d9d69d6f5a0fded10c17db94026edd11c3d230b
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:23:23 +0300

    added generic Entity Framework repository

---
+++ TennisLinks/TennisLinks.Data/EfRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisLinks.Data.Interfaces;
+
+namespace TennisLinks.Data
+{
+    public class EfRepository<T> : IRepository<T>
+        where T : class
+    {
+        protected ITennisLinksDbContext context;
+        protected IDbSet<T> set;
+
+        public EfRepository(ITennisLinksDbContext context)
+        {
+            this.context = context;
+            this.set = context.Set<T>();
+        }
+
+        public IQueryable<T> All()
+        {
+            return this.set;
+        }
+
+        public T GetById(object id)
+        {
+            return this.set.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            this.ChangeState(entity, EntityState.Added);
+        }
+
+        public void Update(T entity)
+        {
+            this.ChangeState(entity, EntityState.Modified);
+        }
+
+        public void Delete(T entity)
+        {
+            this.ChangeState(entity, EntityState.Deleted);
+        }
+
+        public int SaveChanges()
+        {
+            return this.SaveChanges();
+        }
+
+        private void ChangeState(T entity, EntityState state)
+        {
+            var entry = this.context.Entry(entity);
+            entry.State = state;
+        }
+    }
+}
---
+++ TennisLinks/TennisLinks.Data/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace TennisLinks.Data.Interfaces
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        IQueryable<T> All();
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        int SaveChanges();
+    }
+}
