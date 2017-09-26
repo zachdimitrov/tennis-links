4817b767e701e27213d28306517f7fdaacf3fc17
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:15:37 +0300

    created assembly for Infrastrructure items

---
+++ TennisLinks/TennisLinks.Web.Infrastructure/Constants/Assemblies.cs
@@ -0,0 +1,9 @@
+﻿namespace TennisLinks.Web.Infrastructure.Constants
+{
+    public class Assemblies
+    {
+        public const string Infrastructure = "TennisLinks.Web.Infrastructure";
+        public const string Validators = "TennisLinks.Web.Infrastructure";
+        public const string ViewModels = "TennisLinks.Web";
+    }
+}
