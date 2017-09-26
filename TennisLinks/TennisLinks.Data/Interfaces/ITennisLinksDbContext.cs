938b3d99a65eb315912e0f565609d993e2a86d68
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:30:09 +0300

    added dbcontext files

---
+++ TennisLinks/TennisLinks.Data/Interfaces/ITennisLinksDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using TennisLinks.Models;
+
+namespace TennisLinks.Data.Interfaces
+{
+    public interface ITennisLinksDbContext
+    {
+        IDbSet<Message> Messages { get; set; }
+
+        IDbSet<User> Users { get; set; }
+
+        IDbSet<Club> Clubs { get; set; }
+
+        IDbSet<City> Cities { get; set; }
+
+        IDbSet<T> Set<T>() where T : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+    }
+}
