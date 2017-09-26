3e9437accfdad5c26d33029a3d1df6775b7a0c1b
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:18:59 +0300

    created city model

---
+++ TennisLinks/TennisLinks.Models/City.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TennisLinks.Models.Interfaces;
+
+namespace TennisLinks.Models
+{
+    public class City : IEntity
+    {
+        private ICollection<User> users;
+
+        private ICollection<Club> clubs;
+
+        public City()
+        {
+            this.users = new HashSet<User>();
+            this.clubs = new HashSet<Club>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users
+        {
+            get { return this.users; }
+            set { this.users = value; }
+        }
+
+        public virtual ICollection<Club> Clubs
+        {
+            get { return this.clubs; }
+            set { this.clubs = value; }
+        }
+    }
+}
