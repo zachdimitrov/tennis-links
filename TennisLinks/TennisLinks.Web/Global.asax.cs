93f6b914cf5950f8721e04f7f151321a30217d55
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:12:45 +0300

    added configurations for ninject auto binding

--- TennisLinks/TennisLinks.Web/Global.asax.cs
+++ TennisLinks/TennisLinks.Web/Global.asax.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TennisLinks.Data;
+using TennisLinks.Data.Migrations;
+using TennisLinks.Web.App_Start;
+using TennisLinks.Web.Infrastructure.ModelBinders;
 
 namespace TennisLinks.Web
 {
@@ -12,10 +13,15 @@ namespace TennisLinks.Web
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TennisLinksDbContext, Configuration>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutoMapperConfig.RegisterMappings();
+            ModelBinderProviders.BinderProviders.Add(new EntityModelBinderProvider());
         }
     }
 }
