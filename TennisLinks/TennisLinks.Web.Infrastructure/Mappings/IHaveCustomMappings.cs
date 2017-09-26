0459d3104268ba92aca7dd75ddf85760ca811468
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:31:55 +0300

    mappings creation files

---
+++ TennisLinks/TennisLinks.Web.Infrastructure/Mappings/IHaveCustomMappings.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace TennisLinks.Web.Infrastructure.Mappings
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
---
+++ TennisLinks/TennisLinks.Web.Infrastructure/Mappings/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace TennisLinks.Web.Infrastructure.Mappings
+{
+    public interface IMapFrom<T>
+    {
+    }
+}
