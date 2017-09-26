d171f811b972e4780c6e96facf8346c1d7c475e1
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:21:20 +0300

    added message model

---
+++ TennisLinks/TennisLinks.Models/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TennisLinks.Models.Interfaces;
+
+namespace TennisLinks.Models
+{
+    public class Message : IEntity
+    {
+        public Message()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength = 10)]
+        public string Content { get; set; }
+
+        public DateTime SentOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string ContactId { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual User Contact { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
