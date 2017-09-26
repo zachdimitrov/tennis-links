7a5b77390c7931a42fc11e14c380f3cfce922147
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:30:35 +0300

    created entity framework db configuration

---
+++ TennisLinks/TennisLinks.Data/Migrations/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace TennisLinks.Data.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<TennisLinksDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "TennisLinks.Data.TennisLinksDbContext";
+        }
+
+        protected override void Seed(TennisLinksDbContext context)
+        {
+
+        }
+    }
+}
