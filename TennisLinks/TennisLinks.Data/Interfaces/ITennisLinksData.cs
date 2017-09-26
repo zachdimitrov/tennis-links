8a0f4a1dac8d88c204b7adf488f7076981987106
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:29:40 +0300

    added data files

---
+++ TennisLinks/TennisLinks.Data/Interfaces/ITennisLinksData.cs
@@ -0,0 +1,12 @@
+﻿using TennisLinks.Data.Repositories;
+using TennisLinks.Models;
+
+namespace TennisLinks.Data.Interfaces
+{
+    public interface ITennisLinksData
+    {
+        IRepository<Message> Messages { get; }
+
+        IUserRepository Users { get; }
+    }
+}
