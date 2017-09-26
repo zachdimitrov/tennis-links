1038edaeb009cdd7390d9014a00b507b50799d5f
zachdimitrov <zach_dimitrov@yahoo.com>
2017-09-26 23:30:57 +0300

    added user repository

---
+++ TennisLinks/TennisLinks.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisLinks.Models;
+
+namespace TennisLinks.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetByUsername(string username);
+    }
+}
---
+++ TennisLinks/TennisLinks.Data/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+
+namespace TennisLinks.Data.Repositories
+{
+    public class UsersRepository : EfRepository<User>, IUserRepository
+    {
+        public UsersRepository(ITennisLinksDbContext data)
+            : base(data)
+        {
+        }
+
+        public User GetByUsername(string username)
+        {
+            return this.context.Users.Find(username);
+        }
+    }
+}
