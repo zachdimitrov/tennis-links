de5c1448c223f2584a6b2644bc45d2f663a2ff97
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-15 19:26:27 +0300

    create binding models for Club and Playtime

---
+++ TennisLinks/TennisLinks.Web/Models/Home/PlayTimeBindModel.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using TennisLinks.Models;
+using TennisLinks.Web.Infrastructure;
+
+namespace TennisLinks.Web.Models.Home
+{
+    public class PlayTimeBindModel : IMap<PlayTime>, IHaveCustomMappings
+    {
+        public string Time { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Time}";
+        }
+
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<PlayTime, PlayTimeBindModel>()
+                .ForMember(viewModel => viewModel.Time, cfg => cfg.MapFrom(model => model.Time.ToString()));
+        }
+    }
+}
