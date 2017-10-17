c18199a353aefbbacc38bf379a80509f67e5f40e
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 23:40:40 +0300

    Test PlayTime Service

---
+++ TennisLinks/TennisLinks.Web.Tests/Services/PlayTimeServiceTests/ConstructorShould.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.PlayTimeServiceTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Return_PlayTimeService_Instance_WhenParametersSetCorrectly()
+        {
+            // Arrange
+            var playTimesRepoMock = new Mock<IEfRepository<PlayTime>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            // Act
+            var playTimeService = new PlayTimeService(playTimesRepoMock.Object, uowMock.Object);
+
+            // Assert
+            Assert.IsNotNull(playTimeService);
+        }
+    }
+}
---
+++ TennisLinks/TennisLinks.Web.Tests/Services/PlayTimeServiceTests/UpdateShould.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.PlayTimeServiceTests
+{
+    [TestFixture]
+    public class UpdateShould
+    {
+        [Test]
+        public void ReturnProper_UpdatePlayTime_ResultFromCommitMethod()
+        {
+            // Arrange
+            var playTimesRepoMock = new Mock<IEfRepository<PlayTime>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            playTimesRepoMock.Setup(c => c.Update(It.IsAny<PlayTime>())).Verifiable();
+            uowMock.Setup(u => u.Commit()).Returns(1);
+
+            var playTimeService = new PlayTimeService(playTimesRepoMock.Object, uowMock.Object);
+
+            // Act
+            var result = playTimeService.Update(It.IsAny<PlayTime>());
+
+            // Assert
+            Assert.That(result.Equals(1));
+        }
+    }
+}
