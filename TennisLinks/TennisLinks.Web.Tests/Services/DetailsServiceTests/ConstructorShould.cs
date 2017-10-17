27163115510549dcf51d22c0a25e43cbfebdb30c
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 23:39:27 +0300

    Test Details Service

---
+++ TennisLinks/TennisLinks.Web.Tests/Services/DetailsServiceTests/ConstructorShould.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.DetailsServiceTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Return_DetailsService_Instance_WhenParametersSetCorrectly()
+        {
+            // Arrange
+            var detailsRepoMock = new Mock<IEfRepository<Details>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            // Act
+            var detailsService = new DetailsService(detailsRepoMock.Object, uowMock.Object);
+
+            // Assert
+            Assert.IsNotNull(detailsService);
+        }
+    }
+}
