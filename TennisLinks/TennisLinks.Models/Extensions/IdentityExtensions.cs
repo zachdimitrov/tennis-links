d9d75b8e9ca95482379189eebfa0e1a25c5206e6
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 14:07:28 +0300

    created Identity extension to access Details Id in User

---
+++ TennisLinks/TennisLinks.Models/Extensions/IdentityExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace TennisLinks.Models.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetDetailsId(this IIdentity identity)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst("DetailsId");
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
