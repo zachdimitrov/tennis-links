9327a8b845f4a6e1fff9210ef28b34a7dbd39555
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 23:38:16 +0300

    Test Controllers

---
+++ TennisLinks/TennisLinks.Web.Tests/Controllers/AdministrationTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TennisLinks.Models;
+using TennisLinks.Services.Interfaces;
+using TennisLinks.Web.Areas.Administration.Controllers;
+using TennisLinks.Web.Areas.Administration.Models;
+
+namespace TennisLinks.Web.Tests.Controllers.AdministrationTests
+{
+    [TestFixture]
+    public class IndexShould
+    {
+        [Test]
+        public void ReturnCorrect_View()
+        {
+            //// Arrange
+            //var userServiceMock = new Mock<IUserService>();
+            //var mapperMock = new Mock<IMapper>();
+
+            //var controllerUT = new ManageUserDetailsController(userServiceMock.Object, mapperMock.Object);
+
+            //mapperMock.Setup(m => m.Map<object>(It.IsAny<object>())).Verifiable();
+            //userServiceMock.Setup(u => u.GetAll()).Returns(It.IsAny<IQueryable<User>>());
+
+            //// Act
+            //var result = controllerUT.Index() as ViewResult;
+
+            //// Assert
+            //Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
