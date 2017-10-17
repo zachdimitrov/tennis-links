324478294273b10328b7e3bb5a16a09977f910e9
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 23:39:09 +0300

    Test Club Service

---
+++ TennisLinks/TennisLinks.Web.Tests/Services/ClubServiceTests/ConstructorShould.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.ClubSericeTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Return_ClubService_Instance_WhenParametersSetCorrectly()
+        {
+            // Arrange
+            var clubsRepoMock = new Mock<IEfRepository<Club>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            // Act
+            var clubService = new ClubService(clubsRepoMock.Object, uowMock.Object);
+
+            // Assert
+            Assert.IsNotNull(clubService);
+        }
+    }
+}
---
+++ TennisLinks/TennisLinks.Web.Tests/Services/ClubServiceTests/GetAllShould.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.ClubSericeTests
+{
+    [TestFixture]
+    public class GetAllShould
+    {
+        [Test]
+        public void ReturnProper_Clubs_Collection()
+        {
+            // Arrange
+            var clubsRepoMock = new Mock<IEfRepository<Club>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            IEnumerable<Club> collection = new List<Club>();
+            clubsRepoMock.Setup(c => c.All).Returns(() =>
+            {
+                return collection.AsQueryable();
+            });
+
+            var clubService = new ClubService(clubsRepoMock.Object, uowMock.Object);
+
+            // Act
+            IEnumerable<Club> clubs = clubService.GetAll();
+
+            // Assert
+            Assert.That(clubs, Is.EqualTo(collection));
+        }
+    }
+}
