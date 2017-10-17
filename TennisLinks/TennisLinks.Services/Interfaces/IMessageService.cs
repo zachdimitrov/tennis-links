3422bb91d6b5aa1dba210434eda973773a2519d0
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 14:05:40 +0300

    created message service feature

---
+++ TennisLinks/TennisLinks.Services/Interfaces/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using TennisLinks.Models;
+
+namespace TennisLinks.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        int Add(Message message);
+        int Delete(Message message);
+        IQueryable<Message> GetAll();
+        int Update(Message message);
+    }
+}
---
+++ TennisLinks/TennisLinks.Web/Controllers/MessagesController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+using TennisLinks.Services.Interfaces;
+using TennisLinks.Web.Models.Messages;
+using TennisLinks.Web.Infrastructure;
+using System.Net;
+
+namespace TennisLinks.Web.Controllers
+{
+    [Authorize]
+    public class MessagesController : Controller
+    {
+        private readonly IMapper mapper;
+        private readonly IUserService userService;
+        private readonly IMessageService messageService;
+
+        public MessagesController(
+            IMapper mapper,
+            IUserService userService,
+            IMessageService messageService)
+        {
+            this.mapper = mapper;
+            this.userService = userService;
+            this.messageService = messageService;
+        }
+
+        public ActionResult All()
+        {
+            return this.View();
+        }
+
+        public ActionResult Received()
+        {
+            var userName = this.User.Identity.GetUserName();
+            var receivedMessages = this.messageService
+                .GetAll()
+                .Where(m => m.Recipient == userName)
+                .MapTo<MessageViewModel>()
+                .ToList();
+
+            return this.View(receivedMessages);
+        }
+
+        public ActionResult Sent()
+        {
+            var userName = this.User.Identity.GetUserName();
+            var sentMessages = this.messageService
+                .GetAll()
+                .Where(m => m.Author == userName)
+                .MapTo<MessageViewModel>()
+                .ToList();
+
+            return this.View(sentMessages);
+        }
+
+        [HttpGet]
+        public ActionResult Write(string recipient)
+        {
+            var userName = this.User.Identity.GetUserName();
+
+            var model = new MessageViewModel()
+            {
+                Recipient = recipient,
+                Author = userName
+            };
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Write(MessageViewModel model)
+        {
+            var userName = this.User.Identity.GetUserName();
+            var message = new TennisLinks.Models.Message()
+            {
+                Author = userName,
+                Recipient = model.Recipient,
+                Title = model.Title,
+                Content = model.Content
+            };
+
+            var result = this.messageService.Add(message);
+
+            if (result > 0)
+            {
+                return this.RedirectToAction("Sent");
+            }
+
+            return this.MessageValidationFailure();
+        }
+
+        private HttpStatusCodeResult MessageValidationFailure()
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Validation failed! Message was not sent!");
+        }
+    }
+}
