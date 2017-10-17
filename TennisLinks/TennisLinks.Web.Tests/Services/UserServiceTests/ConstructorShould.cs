c33c044a27ac2c362ff62b9cfe0ed046ff15acb8
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 23:41:17 +0300

    test User Service

---
+++ TennisLinks/TennisLinks.Web.Tests/Services/UserServiceTests/ConstructorShould.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.UserServiceTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Return_UserService_Instance_WhenParametersSetCorrectly()
+        {
+            // Arrange
+            var usersRepoMock = new Mock<IEfRepository<User>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            // Act
+            var userService = new UserService(usersRepoMock.Object, uowMock.Object);
+
+            // Assert
+            Assert.IsNotNull(userService);
+        }
+    }
+}
---
+++ TennisLinks/TennisLinks.Web.Tests/Services/UserServiceTests/UserMock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisLinks.Models;
+
+namespace TennisLinks.Web.Tests.Services.UserServiceTests
+{
+    class UserMock : User
+    {
+        public new string Id { get; set; }
+    }
+}
