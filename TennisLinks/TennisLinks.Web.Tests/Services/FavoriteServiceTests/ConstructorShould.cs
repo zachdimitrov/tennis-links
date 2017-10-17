4834be7d4f0d0821b14efe56d04705996850f3e8
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 23:39:50 +0300

    Test Favorites Service

---
+++ TennisLinks/TennisLinks.Web.Tests/Services/FavoriteServiceTests/ConstructorShould.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.FavoriteSericeTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Return_FavoriteService_Instance_WhenParametersSetCorrectly()
+        {
+            // Arrange
+            var favoriteRepoMock = new Mock<IEfRepository<Favorite>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            // Act
+            var favoriteService = new FavoriteService(favoriteRepoMock.Object, uowMock.Object);
+
+            // Assert
+            Assert.IsNotNull(favoriteService);
+        }
+    }
+}
---
+++ TennisLinks/TennisLinks.Web.Tests/Services/FavoriteServiceTests/UpdateShould.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisLinks.Data.Interfaces;
+using TennisLinks.Models;
+using TennisLinks.Services;
+
+namespace TennisLinks.Web.Tests.Services.FavoritesSericeTests
+{
+    [TestFixture]
+    public class UpdateShould
+    {
+        [Test]
+        public void ReturnProper_UpdateFavorite_ResultFromCommitMethod()
+        {
+            // Arrange
+            var favoritesRepoMock = new Mock<IEfRepository<Favorite>>();
+            var uowMock = new Mock<ISaveContext>();
+
+            favoritesRepoMock.Setup(c => c.Update(It.IsAny<Favorite>())).Verifiable();
+            uowMock.Setup(u => u.Commit()).Returns(1);
+
+            var favoriteService = new FavoriteService(favoritesRepoMock.Object, uowMock.Object);
+
+            // Act
+            var result = favoriteService.Add(It.IsAny<Favorite>());
+
+            // Assert
+            Assert.That(result.Equals(1));
+        }
+    }
+}
