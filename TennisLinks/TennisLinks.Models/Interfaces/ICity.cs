3b1dceb30e5630ffddff1292ae0705826807c256
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-10-17 23:37:51 +0300

    Add interfaces to Data Models

---
+++ TennisLinks/TennisLinks.Models/Interfaces/ICity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TennisLinks.Models.Interfaces
+{
+    public interface ICity
+    {
+        ICollection<Club> Clubs { get; set; }
+        string Name { get; set; }
+        ICollection<Details> UserDetails { get; set; }
+    }
+}
---
+++ TennisLinks/TennisLinks.Models/Interfaces/IPlayTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TennisLinks.Models.Enumerations;
+
+namespace TennisLinks.Models.Interfaces
+{
+    public interface IPlayTime
+    {
+        TimeOfDay Time { get; set; }
+        ICollection<Details> UserDetails { get; set; }
+    }
+}
